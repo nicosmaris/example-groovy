5ce802c3e096390ca10f586a4f623683251c667f
Nicos Maris <nicos.maris@gmail.com>
2016-12-06 21:01:51 +0200

    testing average rate of HTTP 200

--- src/test/groovy/org/jacoco/examples/maven/groovy/DisplayDriver.groovy
+++ src/test/groovy/org/jacoco/examples/maven/groovy/DisplayDriver.groovy
@@ -7,14 +7,14 @@ import org.openqa.selenium.firefox.FirefoxProfile
 
 class DisplayDriver {
     private WebDriver browser_with_debug_level;
-    public DisplayDriver(display){
+    DisplayDriver(display){
         String cwd = System.getProperty("user.dir");
         File pathToBinary = new File(cwd, "ui/firefox/firefox");
         FirefoxBinary ffox = new FirefoxBinary(pathToBinary);
         ffox.setEnvironmentProperty("DISPLAY", ":" + display);
         browser_with_debug_level = new FirefoxDriver(ffox, new FirefoxProfile());
     }
-    public close(){
+    void close(){
         browser_with_debug_level.close();
         browser_with_debug_level.quit();
     }
--- src/test/groovy/org/jacoco/examples/maven/groovy/SearchTest.groovy
+++ src/test/groovy/org/jacoco/examples/maven/groovy/SearchTest.groovy
@@ -1,11 +1,16 @@
 package org.jacoco.examples.maven.groovy
 
+import io.gatling.core.scenario.Simulation
 import org.junit.After
 
 import static org.junit.Assert.*
 
 import org.junit.Before
 import org.junit.Test
+import io.gatling.core.Predef.*
+import io.gatling.http.Predef.*
+import scala.concurrent.duration.*
+
 
 class SearchTest {
 	private driver;
@@ -18,11 +23,11 @@ class SearchTest {
         driver.close();
     }
 	@Test
-	void testGetMessageFalse() {
+	void testPositive() {
         sleep(2000);
 	}
 	@Test
-	void testGetMessageTrue() {
+	void testNegative1() {
         sleep(2000);
 	}
 }
